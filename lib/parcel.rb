4d6fc29f33b8b518d901cc60fb8d1ffba5a83cff
Sean St. Quentin <sean@iproperty.com.au>
2010-04-07 14:57:06 +1000

    Bugfix, wasn't splitting directory correctly.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -49,7 +49,7 @@ module Parcel
     
     merge_opts = framework_opts.merge({ :class_name => underscore(reference.class.name) }).merge(options)
     
-    merge_opts[:id] = ("%08d" % merge_opts[:id].to_s).scan(/..../)
+    merge_opts[:id] = ("%08d" % merge_opts[:id].to_s.to_i).scan(/..../).join("/")
     
     merge_opts.to_a.sort_by { |(k,v)| -k.to_s.length }.each do |(k,v)|
       new_path = new_path.gsub(":#{k}", v.to_s)
