3acc6df975abbef7b61c10b6d257cce2cfa0e649
Sean St. Quentin <sean.stquentin@gmail.com>
2011-11-23 17:38:46 +1100

    Add a DummyInterface to the specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ $: << File.join(File.dirname(__FILE__), "..", "lib")
 require 'parcel'
 
 class DummyStorage < Parcel::Storage::Base
+end
 
+class DummyInterface < Parcel::Interfaces::Base
 end
 
 Parcel.register_storage :dummy, DummyStorage
+Parcel.register_interface :dummy, DummyInterface
