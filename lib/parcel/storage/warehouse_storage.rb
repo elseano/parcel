6836911e06150a532eb2e3c3ef46f41cc08c7ea8
Sean St. Quentin <sean.stquentin@gmail.com>
2011-11-25 13:37:29 +1100

    Update the Warehouse storage to persist its state into an active record object.

--- lib/parcel/storage/warehouse_storage.rb
+++ lib/parcel/storage/warehouse_storage.rb
@@ -9,6 +9,9 @@ module Parcel
 		# To put an item into longer term storage, run #warehouse!. Fetching data will
 		# try the fast storage first, and the warehouse second. If data is accessed from
 		# the warehouse, it is placed into the fast storage.
+		#
+		# Warehouse state is stored on a warehoused active record attribute. Override this
+		# class and its registration to integrate with other storage mechanisms.
 		class WarehouseStorage < Base
 
 			def fast_storage
@@ -20,28 +23,26 @@ module Parcel
 			end
 
 			def write(data_stream)
-				raise WarehousedError if warehoused?
+				raise(WarehousedError, "Cannot save a warehoused file") if warehoused?
 				fast_storage.write(data_stream)
 			end
 
-			# Is the parcel currently warehoused?
+			# Is the parcel currently warehoused? Delegates to the warehoused indicator option.
 			def warehoused?
-				@warehoused == true
+				read_warehouse_state
 			end
 
 			def read
-				@warehoused = nil
-
-				fast_storage.read do |input|
-					yield input
-					@warehoused = false
-				end
-
-				return if @warehoused == nil
-
-				warehouse_storage.read do |input|
-					yield input
-					@warehoused = true
+				if warehoused?
+					warehouse_storage.read do |input|
+						yield input
+						@warehoused = true
+					end
+				else
+					fast_storage.read do |input|
+						yield input
+						@warehoused = false
+					end
 				end
 			end
 
@@ -51,15 +52,19 @@ module Parcel
 				warehouse_storage.delete
 			end
 
-			# Moves the data asset into the warehouse.
+			# Moves the data asset into the warehouse. This can only warehouse a saved file,
+			# so make sure the parcel has been written back to the fast storage before
+			# warehousing. 
 			def warehouse!
 				return if warehoused?
 				fast_storage.read do |input|
 					warehouse_storage.write(input)
+					write_warehouse_state(true)
 				end
 
+				raise(EmptyRepository, "Nothing to warehouse") unless warehoused?
+
 				fast_storage.delete
-				@warehoused = true
 
 				object.send(options[:after_warehouse]) if options[:after_warehouse]
 			end
@@ -69,12 +74,23 @@ module Parcel
 
 				warehouse_storage.read do |input|
 					fast_storage.write(input)
+					write_warehouse_state(false)
 				end
 
 				warehouse_storage.delete
-				@warehoused = false
 
 				object.send(options[:after_retrieve]) if options[:after_retrieve]
+			end
+
+			protected
+
+			def read_warehouse_state
+				object.warehoused?
+			end
+
+			def write_warehouse_state(value)
+				object.warehouse = value
+				object.class.update_all("warehoused = #{value.to_s}", "id = #{object.id}")
 			end
 
 		end
